f4729614b746d486de8858401e55f8470a6a9194
Alif Jafar <alifjafar@gmail.com>
2018-06-26 13:33:57 +0700

    Fixing H1 Single Post

--- resources/views/blog/posts/single.blade.php
+++ resources/views/blog/posts/single.blade.php
@@ -23,12 +23,16 @@
                 </div>
                 <div class="card-body post-wrapper">
                     <div class="post-header mb-3">
+                        <div class="row">
+                            <div class="col-md-6">
                         <span class="label-cat">
                             <a href="{{ route('category_post', $post->category->slug) }}">{{ $post->category->name }}</a>
                             <span class="label-date" style="font-size:12px">/ {{ date('M j, Y', strtotime( $post->created_at )) }}
                                 / <a href="{{ Request::url()}}#disqus_thread">0 Komentar</a>
                             </span>
                         </span>
+                    </div>
+                    <div class="col-md-6">
                         <span class="float-right">
                             <span class="card-author text-muted">
                                     Ditulis oleh :
@@ -37,6 +41,8 @@
                                 <img src="{{ $post->authorId->avatar }}" alt="$post->authorId->name" class="avatar-post"> {{ $post->authorId->name }}
                             </a>
                         </span>
+                    </div>
+                </div>
                         <h1 class="card-title text--black">{{ $post->title }}</h1>
                     </div>
                     <hr>
